b2a506a4f9cd7ed2ce013d56a8d8881e464a99bc
Enrique1990 <ecarrillo@techlaunch.io>
2018-09-17 20:22:16 -0400

    first commit

---
+++ firstname.js
@@ -0,0 +1,12 @@
+var firstname = document.querySelectorAll(".firstname")
+var lastname = document.querySelectorAll(".lastname")
+console.log(firstname)
+console.log(lastname)
+  
+var num = 5+3+7;
+console.log(num);
+var operone = eight + six + two; 
+console.log [operone];
+
+
+var longString = "the name";
